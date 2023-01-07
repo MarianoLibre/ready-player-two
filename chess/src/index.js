27961e9010e8034918db35d9ab404baac63374a5
MarianoLibre <mariano.macri@mercadolibre.com>
2023-01-07 20:31:52 -0300

    Add pieces and highlighting

--- chess/src/index.js
+++ chess/src/index.js
@@ -5,13 +5,11 @@ const root = document.getElementById("root")
 const board = document.createElement("div")
 board.setAttribute("id", "board")
 
-const squares = [
-]
+const squares = {}
 
 for (let row = 0; row < 10; row++) {
   for (let col = 0; col < 10; col++) {
     const sqr = document.createElement("div")
-    sqr.setAttribute("class", "square")
 
     if (row === 0 || row === 9 || col === 0 || col === 9) {
       sqr.classList += " border"
@@ -38,7 +36,17 @@ for (let row = 0; row < 10; row++) {
     }
 
     if (col !== 0 && col !== 9 && row !== 0 && row !== 9) {
-      squares.push(sqr)
+      sqr.setAttribute("class", "square")
+      // hidden squares will be highlighted to show possible movements
+      const text = document.createElement("div")
+      text.setAttribute("class", "text")
+      sqr.appendChild(text)
+      const hidden = document.createElement("div")
+      hidden.setAttribute("class", "hidden")
+      sqr.appendChild(hidden)
+
+      squares[" abcdefgh"[col] + (9 - row)] = text
+
       if ((col + row) % 2 === 0) {
         sqr.classList += " white"
       } else {
@@ -49,5 +57,39 @@ for (let row = 0; row < 10; row++) {
     board.appendChild(sqr)
   }
 }
+
+squares["a1"].innerText = "R"
+squares["b1"].innerText = "N"
+squares["c1"].innerText = "B"
+squares["d1"].innerText = "Q"
+squares["e1"].innerText = "K"
+squares["f1"].innerText = "B"
+squares["g1"].innerText = "N"
+squares["h1"].innerText = "R"
+squares["a2"].innerText = "P"
+squares["b2"].innerText = "P"
+squares["c2"].innerText = "P"
+squares["d2"].innerText = "P"
+squares["e2"].innerText = "P"
+squares["f2"].innerText = "P"
+squares["g2"].innerText = "P"
+squares["h2"].innerText = "P"
+
+squares["a8"].innerText = "R"
+squares["b8"].innerText = "N"
+squares["c8"].innerText = "B"
+squares["d8"].innerText = "Q"
+squares["e8"].innerText = "K"
+squares["f8"].innerText = "B"
+squares["g8"].innerText = "N"
+squares["h8"].innerText = "R"
+squares["a7"].innerText = "P"
+squares["b7"].innerText = "P"
+squares["c7"].innerText = "P"
+squares["d7"].innerText = "P"
+squares["e7"].innerText = "P"
+squares["f7"].innerText = "P"
+squares["g7"].innerText = "P"
+squares["h7"].innerText = "P"
 
 root.appendChild(board)
